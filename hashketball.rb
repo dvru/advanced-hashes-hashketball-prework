2d6983002d04667a7ec33cd6848c223e585457b0
Damini Varu <damvaru4@gmail.com>
2019-08-31 01:05:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,217 +1,161 @@
-#require "pry"
 
 def game_hash
-  game_hash = {
-    :home => {
-      :team_name => "Brooklyn Nets",
-      :colors => ["Black", "White"],
-      :players => {
+  hash = {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
         "Alan Anderson" => {
-          :number => 0,
-          :shoe => 16,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
         },
         "Reggie Evans" => {
-          :number => 30,
-          :shoe => 14,
-          :points => 12,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 12,
-          :blocks => 12,
-          :slam_dunks => 7
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
         },
         "Brook Lopez" => {
-          :number => 11,
-          :shoe => 17,
-          :points => 17,
-          :rebounds => 19,
-          :assists => 10,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 15
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
         },
         "Mason Plumlee" => {
-          :number => 1,
-          :shoe => 19,
-          :points => 26,
-          :rebounds => 12,
-          :assists => 6,
-          :steals => 3,
-          :blocks => 8,
-          :slam_dunks => 5
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
         },
         "Jason Terry" => {
-          :number => 31,
-          :shoe => 15,
-          :points => 19,
-          :rebounds => 2,
-          :assists => 2,
-          :steals => 4,
-          :blocks => 11,
-          :slam_dunks => 1
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
         }
       }
     },
-    :away => {
-      :team_name => "Charlotte Hornets",
-      :colors => ["Turquoise", "Purple"],
-      :players => {
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
         "Jeff Adrien" => {
-          :number => 4,
-          :shoe => 18,
-          :points => 10,
-          :rebounds => 1,
-          :assists => 1,
-          :steals => 2,
-          :blocks => 7,
-          :slam_dunks => 2
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
         },
         "Bismak Biyombo" => {
-          :number => 0,
-          :shoe => 16,
-          :points => 12,
-          :rebounds => 4,
-          :assists => 7,
-          :steals => 7,
-          :blocks => 15,
-          :slam_dunks => 10
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
         },
         "DeSagna Diop" => {
-          :number => 2,
-          :shoe => 14,
-          :points => 24,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 4,
-          :blocks => 5,
-          :slam_dunks => 5
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
         },
         "Ben Gordon" => {
-          :number => 8,
-          :shoe => 15,
-          :points => 33,
-          :rebounds => 3,
-          :assists => 2,
-          :steals => 1,
-          :blocks => 1,
-          :slam_dunks => 0
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
         },
         "Brendan Haywood" => {
-          :number => 33,
-          :shoe => 15,
-          :points => 6,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 22,
-          :blocks => 5,
-          :slam_dunks => 12
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
         }
       }
     }
   }
 end
 
-#def good_practices
- # game_hash.each do |location, team_data|
-    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-  #  binding.pry
-   #   team_data.each do |attribute, data|
-        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-    #    binding.pry
-
-        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-     #   data.each do |data_item|
-      #      binding.pry
-    #  end
-  #  end
-#  end
-#end
-
-#good_practices
-
-def num_points_scored(name)
-  game_hash.each do |side, details|
-    details[:players].each do |names,stats|
-      if name == names
-        return stats[:points]
-      end
-    end
-  end
+def num_points_scored(playerName)
+  game_hash[:home][:players].include?(playerName) ? game_hash[:home][:players][playerName][:points] : game_hash[:away][:players][playerName][:points]
 end
 
-def shoe_size(name)
-  game_hash.each do |side, details|
-    details[:players].each do |names,stats|
-      if name == names
-        return stats[:shoe]
-      end
-    end
-  end
+def shoe_size(playerName)
+  game_hash[:home][:players].include?(playerName) ? game_hash[:home][:players][playerName][:shoe] : game_hash[:away][:players][playerName][:shoe]
 end
 
-def team_colors(team_name)
-  colors = []
-  game_hash.each do |location, details|
-    details.each do |thing, value|
-      if value == team_name
-        colors << game_hash[location][:colors]
-      end
-    end
-  end
-  colors.flatten
+def team_colors(teamName)
+  game_hash[:home][:team_name] == (teamName) ? game_hash[:home][:colors] : game_hash[:away][:colors]
 end
 
 def team_names
-  teams = []
-  game_hash.each do |location, details|
-    details.each do |item, value|
-      if item == :team_name
-        teams << value
-      end
-    end
-  end
-  teams
+  [game_hash[:home][:team_name], game_hash[:away][:team_name]]
 end
 
-def player_numbers (team_name)
-  num_array = []
-  game_hash.each do |location, details|
-    if details[:team_name] == team_name
-      details[:players].each do |name, stats|
-        num_array << stats[:number]
-      end
-    end
-  end
-  num_array
+def player_numbers(teamName)
+  numbers = []
+  (game_hash[:home][:team_name] == teamName ? game_hash[:home] : game_hash[:away])[:players].each {|player, stats|
+    numbers.push(stats[:number])
+  }
+  numbers
 end
 
-def player_stats(player_name)
-  game_hash.each do |location, details|
-    details[:players].each do |name, stats|
-      if player_name == name
-        return stats
-      end
-    end
-  end
+def player_stats(playerName)
+  game_hash[:home][:players].include?(playerName) ? game_hash[:home][:players][playerName] : game_hash[:away][:players][playerName]
 end
+
+
+
 
 def big_shoe_rebounds
-  shoe_size = 0
-  rebounds = 0 
-
-  game_hash.each do |side, details|
-    details[:players].each do |name, stats|
-      if stats[:shoe] > shoe_size
-        shoe_size = stats[:shoe]
+  shoeSize = 0
+  rebounds = 0
+  game_hash.each {|team,atributes|
+    atributes[:players].each {|player, stats|
+      if stats[:shoe] > shoeSize
         rebounds = stats[:rebounds]
       end
-    end
-  end
+    }
+  }
   rebounds
-end 
+end
