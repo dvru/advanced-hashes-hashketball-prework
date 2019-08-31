5867700920790418e2d509487a2ad9ec129cff82
Damini Varu <damvaru4@gmail.com>
2019-08-31 01:04:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,12 @@
-require "pry"
+#require "pry"
 
 def game_hash
   game_hash = {
     :home => {
       :team_name => "Brooklyn Nets",
-      :colors => ['black','white'],
-      :players => [
-        {
-          :name => "Alan Anderson",
+      :colors => ["Black", "White"],
+      :players => {
+        "Alan Anderson" => {
           :number => 0,
           :shoe => 16,
           :points => 22,
@@ -15,10 +14,9 @@ def game_hash
           :assists => 12,
           :steals => 3,
           :blocks => 1,
-          :slam_dunks => 1,
+          :slam_dunks => 1
         },
-        {
-          :name => "Reggie Evans",
+        "Reggie Evans" => {
           :number => 30,
           :shoe => 14,
           :points => 12,
@@ -26,10 +24,9 @@ def game_hash
           :assists => 12,
           :steals => 12,
           :blocks => 12,
-          :slam_dunks => 7,
+          :slam_dunks => 7
         },
-         {
-          :name => "Brook Lopez",
+        "Brook Lopez" => {
           :number => 11,
           :shoe => 17,
           :points => 17,
@@ -37,10 +34,9 @@ def game_hash
           :assists => 10,
           :steals => 3,
           :blocks => 1,
-          :slam_dunks => 15,
+          :slam_dunks => 15
         },
-         {
-          :name => "Mason Plumlee",
+        "Mason Plumlee" => {
           :number => 1,
           :shoe => 19,
           :points => 26,
@@ -48,10 +44,9 @@ def game_hash
           :assists => 6,
           :steals => 3,
           :blocks => 8,
-          :slam_dunks => 5,
+          :slam_dunks => 5
         },
-         {
-          :name => "Jason Terry",
+        "Jason Terry" => {
           :number => 31,
           :shoe => 15,
           :points => 19,
@@ -59,16 +54,15 @@ def game_hash
           :assists => 2,
           :steals => 4,
           :blocks => 11,
-          :slam_dunks => 1,
+          :slam_dunks => 1
         }
-        ]
+      }
     },
-:away => {
+    :away => {
       :team_name => "Charlotte Hornets",
-      :colors => ['Turquoise', 'purple'],
-      :players => [
-        {
-          :name => "Jeff Adrien",
+      :colors => ["Turquoise", "Purple"],
+      :players => {
+        "Jeff Adrien" => {
           :number => 4,
           :shoe => 18,
           :points => 10,
@@ -76,10 +70,9 @@ def game_hash
           :assists => 1,
           :steals => 2,
           :blocks => 7,
-          :slam_dunks => 2,
+          :slam_dunks => 2
         },
-        {
-          :name => "Bismak Biyombo",
+        "Bismak Biyombo" => {
           :number => 0,
           :shoe => 16,
           :points => 12,
@@ -87,10 +80,9 @@ def game_hash
           :assists => 7,
           :steals => 7,
           :blocks => 15,
-          :slam_dunks => 10,
+          :slam_dunks => 10
         },
-        {
-          :name => "DeSagna Diop",
+        "DeSagna Diop" => {
           :number => 2,
           :shoe => 14,
           :points => 24,
@@ -98,10 +90,9 @@ def game_hash
           :assists => 12,
           :steals => 4,
           :blocks => 5,
-          :slam_dunks => 5,
+          :slam_dunks => 5
         },
-        {
-          :name => "Ben Gordon",
+        "Ben Gordon" => {
           :number => 8,
           :shoe => 15,
           :points => 33,
@@ -109,10 +100,9 @@ def game_hash
           :assists => 2,
           :steals => 1,
           :blocks => 1,
-          :slam_dunks => 0,
+          :slam_dunks => 0
         },
-        {
-          :name => "Brendan Haywood",
+        "Brendan Haywood" => {
           :number => 33,
           :shoe => 15,
           :points => 6,
@@ -120,96 +110,108 @@ def game_hash
           :assists => 12,
           :steals => 22,
           :blocks => 5,
-          :slam_dunks => 12,
+          :slam_dunks => 12
         }
-        ]
       }
+    }
   }
 end
 
-def good_practices
-  game_hash.each do |location, team_data|
-    binding.pry
-team_data.each do |attribute, data|
-  binding.pry
-  data.each do |data_item|
-            binding.pry
-      end
-    end
-  end
-end
+#def good_practices
+ # game_hash.each do |location, team_data|
+    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+  #  binding.pry
+   #   team_data.each do |attribute, data|
+        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+    #    binding.pry
+
+        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
+     #   data.each do |data_item|
+      #      binding.pry
+    #  end
+  #  end
+#  end
+#end
+
+#good_practices
 
 def num_points_scored(name)
-  answer = nil
-  game_hash.each do |team, points|
-    players_array = points[:players]
-    players_array.each do |player_points|
-      if player_points[:name] == name
-        answer = player_points[:points]
+  game_hash.each do |side, details|
+    details[:players].each do |names,stats|
+      if name == names
+        return stats[:points]
       end
     end
   end
-answer
 end
 
 def shoe_size(name)
-  answer = nil
-   game_hash.each do |team, shoe|
-    players_array = shoe[:players]
-    players_array.each do |player_shoe|
-      if player_shoe[:name] == name
-        answer = player_shoe[:shoe]
+  game_hash.each do |side, details|
+    details[:players].each do |names,stats|
+      if name == names
+        return stats[:shoe]
       end
     end
   end
-answer
-
 end
 
-def team_colors(team)
-  game_hash.values.each do |team_info|
-    if team_info.has_value?(team) 
-      return team_info[:colors].map(&:capitalize)
+def team_colors(team_name)
+  colors = []
+  game_hash.each do |location, details|
+    details.each do |thing, value|
+      if value == team_name
+        colors << game_hash[location][:colors]
+      end
     end
- end
+  end
+  colors.flatten
 end
 
 def team_names
-  game_hash.values.map {|value| value[:team_name]}
-end
-
-def player_numbers(team)
-game_hash.values.each do |team_info|
-  if team_info.has_value?(team)
-    return team_info[:players].map { |player| player[:number]}
-    end
-  end
-end
-
-
-def player_stats(player_name)
-  player_stats = {}
-  game_hash.each do |team, team_details|
-    team_details[:players].each do |stats|
-      if stats[:name] == player_name
-        stats.delete(:name)
-        player_stats = stats
+  teams = []
+  game_hash.each do |location, details|
+    details.each do |item, value|
+      if item == :team_name
+        teams << value
       end
     end
   end
-  player_stats
+  teams
+end
+
+def player_numbers (team_name)
+  num_array = []
+  game_hash.each do |location, details|
+    if details[:team_name] == team_name
+      details[:players].each do |name, stats|
+        num_array << stats[:number]
+      end
+    end
+  end
+  num_array
+end
+
+def player_stats(player_name)
+  game_hash.each do |location, details|
+    details[:players].each do |name, stats|
+      if player_name == name
+        return stats
+      end
+    end
+  end
 end
 
 def big_shoe_rebounds
-  biggest_shoe = 0 
-  player_rebounds = 0 
-  game_hash.values.each do |team_info|
-    team_info[:players].each do |player|
-      if player[:shoe] > biggest_shoe
-        biggest_shoe = player[:shoe]
-        player_rebounds = player[:rebounds]
+  shoe_size = 0
+  rebounds = 0 
+
+  game_hash.each do |side, details|
+    details[:players].each do |name, stats|
+      if stats[:shoe] > shoe_size
+        shoe_size = stats[:shoe]
+        rebounds = stats[:rebounds]
       end
     end
   end
-  return player_rebounds
-end
+  rebounds
+end 
